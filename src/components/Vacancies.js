725b7ef8dcc89f6a94d51f7833a9fc01e34e207d
Paulo Martins <pfmpaulomartins@gmail.com>
2022-03-16 08:41:58 -0300

    adding link to vacancies and loading state

--- src/components/Vacancies.js
+++ src/components/Vacancies.js
@@ -5,18 +5,29 @@ const API_PATH = 'http://www.mocky.io/v2/5d6fb6b1310000f89166087b';
 
 const Vacancies = () => {
     const [vacancies, setVacancies] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const getUpToDateVacancies = async () => {
-        const { vagas } = await fetch(API_PATH).then(response => response.json());
-        if (!vagas) return;
+        toggleLoader(true);
 
-        const updatedVacancies = vagas.filter((item) => item.ativa).map((vacancie) => {
+        const reponse = await fetch(API_PATH)
+            .then(response => response.json())
+            .catch(() => setErrorMessage('Error ao carregar vagas. Por favor recarregue a página.'));
+
+        toggleLoader(false);
+        setErrorMessage('');
+
+        if (!reponse) return;
+
+        const updatedVacancies = reponse.vagas.filter((item) => item.ativa).map((vacancie) => {
             const formattedAddress = !vacancie.localizacao
                 ? 'Remoto'
                 : `${vacancie.localizacao.bairro} - ${vacancie.localizacao.cidade}, ${vacancie.localizacao.pais}`;
 
             return {
                 position: vacancie.cargo,
+                link: vacancie.link,
                 formattedAddress
             }
         })
@@ -24,8 +35,8 @@ const Vacancies = () => {
         setVacancies(updatedVacancies);
     }
 
-    const onSelectJob = () => {
-        window.open('https://jobs.kenoby.com/elo7', '_blank');
+    const toggleLoader = (isLoading) => {
+        setLoading(isLoading);
     }
 
     useEffect(() => {
@@ -33,18 +44,21 @@ const Vacancies = () => {
     }, []);
 
     return (
-        <section id="vacancies" className="vacancies">
-            <img src={vacanciesBackground} alt="Vagas" className="vacancies__background" />
+        <section id="vacancies" data-testid="vacancies-element" className="vacancies">
+            <img src={vacanciesBackground} alt="Vagas background" className="vacancies__background" />
             <div className="vacancies__title">Vagas em aberto</div>
             <div className="vacancies__list" id="open_jobs">
                 {vacancies.map((item, index) => (
-                    <div key={index} className="vacancies__list-item" onClick={onSelectJob}>
+                    <a href={item.link} key={index} target="_blank" rel="noreferrer" className="vacancies__list-item">
                         <div className="vacancies__list-item-title">{item.position}</div>
                         <div className="vacancies__list-item-text">
                             {item.formattedAddress}
                         </div>
-                    </div>
+                    </a>
                 ))}
+
+                {loading && <div className="">Carregando...</div>}
+                {errorMessage && <div className="vacancies__text-error">{errorMessage}</div>}
             </div>
         </section>
     );
